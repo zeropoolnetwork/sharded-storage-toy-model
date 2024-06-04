f677e7e8059725b10e7872854368518ab52d8f36
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-06-04 11:19:48 +0200

    try to import eddsa poseidon

--- crypto-sdk/src/index.ts
+++ crypto-sdk/src/index.ts
@@ -1,10 +1,3 @@
 import { Barretenberg, Fr } from '@aztec/bb.js';
 
-// import {
-//     derivePublicKey,
-//     signMessage,
-//     verifySignature,
-//     deriveSecretScalar,
-//     packPublicKey,
-//     unpackPublicKey
-// } from "@zk-kit/eddsa-poseidon";
+import { EdDSAPoseidon } from "@zk-kit/eddsa-poseidon";
