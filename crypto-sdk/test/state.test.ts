e86d8761cd45daba1d6729513f92dc873c178bad
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-06-10 16:59:34 +0200

    make the prover test execute nargo prove directly

--- crypto-sdk/test/state.test.ts
+++ crypto-sdk/test/state.test.ts
@@ -5,13 +5,46 @@ import { State, blank_account_tx, blank_file_tx, new_account_tx } from '../src/s
 import { cpus } from 'os';
 import { ShardedStorageSettings, defShardedStorageSettings } from '../src/settings';
 import { blank_mining_tx } from '../src/mining';
-import { RandomOracle, RollupInput, RollupPubInput, Root, main, main_circuit } from '../src/noir_codegen';
+import { RandomOracle, Field, RollupInput, RollupPubInput, Root, circuits, circuits_circuit } from '../src/noir_codegen';
+
+import { parse, stringify } from 'smol-toml';
 
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
 
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
 import { Worker } from 'worker_threads';
 Worker.setMaxListeners(2000);
+
+type ProverToml = {
+      pubhash: Field,
+      input: RollupInput,
+};
+
+export async function prove(nargo_project_path: string, inputs: ProverToml) {
+  const tempDirPath = fs.mkdtempSync(path.join(os.tmpdir(), 'sharded-storage-prover-'));
+  const tempFilePath = path.join(tempDirPath, 'Prover.toml');
+
+  try {
+    fs.writeFileSync(tempFilePath, stringify(inputs));
+    const out = execSync(`
+       set -xe
+       cp -rv ${nargo_project_path}/* "${tempDirPath}"
+       cd "${tempDirPath}"
+       nargo prove --prover-name "${tempFilePath}"
+       find
+   `, { encoding: 'utf8' });
+    console.log(out);
+  } catch (e) {
+    throw e;
+  } finally {
+    execSync("rm -rf ${tempDirPath}");
+  }
+}
 
 describe('State', () => {
 
@@ -39,7 +72,7 @@ describe('State', () => {
         data: new Array(sett.oracle_len).fill("0"),
       } as RandomOracle,
     };
-    const pubInputHash = pub_input_hash(sett, pubInput);
+    const pubInputHash = frToBigInt(pub_input_hash(sett, pubInput)).toString();
 
     console.log("building input");
 
@@ -58,23 +91,30 @@ describe('State', () => {
       new_root: st_root,
     };
 
-    const backend = new BarretenbergBackend(main_circuit);
-    const noir = new Noir(main_circuit, backend);
-    console.log('logs', 'Generating proof... ⌛');
-    const ins = {
-      pubhash: frToNoir(pubInputHash),
-      input: input,
+    const prover_data = {
+      pubhash: pubInputHash,
+      input: input
     };
-    console.log(JSON.stringify(ins));
-    const proof = await noir.generateProof(ins);
-    console.log('logs', 'Generating proof... ✅');
-    console.log('results', proof.proof);
+
+    prove("../circuits/", prover_data);
+
+    // const backend = new BarretenbergBackend(circuits_circuit);
+    // const noir = new Noir(circuits_circuit, backend);
+    // console.log('logs', 'Generating proof... ⌛');
+    // const ins = {
+    //   pubhash: frToNoir(pubInputHash),
+    //   input: input,
+    // };
+    // console.log(JSON.stringify(ins));
+    // const proof = await noir.generateProof(ins);
+    // console.log('logs', 'Generating proof... ✅');
+    // console.log('results', proof.proof);
     // console.log('logs', 'Verifying proof... ⌛');
     // const verification = await noir.verifyProof(proof);
     // expect(verification).toEqual(true);
     // console.log('logs', 'Verifying proof... ✅');
 
     bb.destroy();
-  }, 60 * 1000); // 60 seconds
+  }, 10 * 60 * 1000); // 10 minutes
 
 });
