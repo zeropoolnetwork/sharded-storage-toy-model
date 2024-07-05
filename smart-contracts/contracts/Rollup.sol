14ed4c77a8e66ec559493785ad51b1cc6a54d325
Dmitry Vdovin <voidxnull@gmail.com>
2024-07-05 06:35:37 +0400

    Fix random oracle in smart contract, increase BLOCKNUMBER_MAX_DELAY

--- smart-contracts/contracts/Rollup.sol
+++ smart-contracts/contracts/Rollup.sol
@@ -15,7 +15,7 @@ contract Rollup {
     // BN256 group order
     uint256 constant R = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
-    uint256 constant BLOCKNUMBER_MAX_DELAY = 10;
+    uint256 constant BLOCKNUMBER_MAX_DELAY = 100;
 
     UltraVerifier public verifier;
     address public owner;
@@ -51,8 +51,9 @@ contract Rollup {
 
         // use block hashes as random oracle, should be replaced to randao
         uint256[RANDOM_ORACLE_SIZE] memory oracle;
-        for (uint256 i = _now-RANDOM_ORACLE_SIZE+1; i <= _now; i++) {
-            oracle[i] = uint256(blockhash(i));
+        uint ro_base = _now - RANDOM_ORACLE_SIZE + 1;
+        for (uint256 i = 0; i < RANDOM_ORACLE_SIZE; i++) {
+            oracle[i] = uint256(blockhash(ro_base+i))%R;
         }
 
         uint256 h = uint256(keccak256(abi.encodePacked(root, new_root, _now, _now-RANDOM_ORACLE_SIZE+1, oracle)))%R;
